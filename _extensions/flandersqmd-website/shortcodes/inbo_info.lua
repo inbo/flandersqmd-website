c48e80dae654e01bb1697a14492f1fd4b29ca913
ElsLommelen <els.lommelen@inbo.be>
2026-05-29 17:09:33 +0200

    move client and cooperation info to new client.lua (not yet implemented in standard template)

--- _extensions/flandersqmd-website/shortcodes/inbo_info.lua
+++ _extensions/flandersqmd-website/shortcodes/inbo_info.lua
@@ -139,32 +139,6 @@ return {
     end
 
     z = z .. '</div>'
-    if not is_empty(meta.flandersqmd.client) then
-      z = z .. '<div>' .. '<div class="quarto-title-meta-heading">' ..
-        meta.translation.client .. '</div>' ..
-        '<div class="quarto-title-meta-contents">'
-      for i, client in pairs(meta.flandersqmd.client) do
-        z = z .. pandoc.utils.stringify(client) .. '<br>'
-      end
-      if not is_empty(meta.flandersqmd.clientlogo) then
-        z = z .. '<img src="' ..
-          pandoc.utils.stringify(meta.flandersqmd.clientlogo) .. '" class="client">'
-      end
-      z = z .. '</div>' .. '</div>'
-    end
-    if not is_empty(meta.flandersqmd.cooperation) then
-      z = z .. '<div>' .. '<div class="quarto-title-meta-heading">' ..
-        meta.translation.cooperation .. '</div>' ..
-        '<div class="quarto-title-meta-contents">'
-      for i, cooperation in pairs(meta.flandersqmd.cooperation) do
-        z = z .. pandoc.utils.stringify(cooperation) .. '<br>'
-      end
-      if not is_empty(meta.flandersqmd.cooperationlogo) then
-        z = z .. '<img src="' ..
-          pandoc.utils.stringify(meta.flandersqmd.cooperationlogo) .. '" class="client">'
-      end
-      z = z .. '</div>' .. '</div>'
-    end
 
 --[[finalise]]
     z = z .. '</div>'
